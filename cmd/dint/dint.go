a6a15f17aaba3033e83e472a94b07e98b3e045cc
Aiden Arnkels-Webb <Aiden@Aidenwebb.com>
2024-10-16 14:34:06 +0100

    Rewrite output format for better readability

--- cmd/dint/dint.go
+++ cmd/dint/dint.go
@@ -9,7 +9,6 @@ import (
 	"github.com/rootwireltd/dint/common/dns_resolver"
 	"github.com/rootwireltd/dint/common/spf_analyser"
 	"log"
-	"strings"
 )
 
 //TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
@@ -70,14 +69,14 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error fetching SPF records: %v", err)
 	}
-	spfTestResults, spfSummary := spf_analyser.CheckSPFRecord(spfRecords)
+	spfTestResults, _ := spf_analyser.CheckSPFRecord(spfRecords)
 
 	// Fetch DMARC Records
 	dmarcRecords, err := dns_resolver.LookupDMARCRecords(*domain)
 	if err != nil {
 		log.Fatalf("Error fetching DMARC records: %v", err)
 	}
-	dmarcTestResults, dmarcSummary := dmarc_analyser.CheckDMARCRecord(dmarcRecords)
+	dmarcTestResults, _ := dmarc_analyser.CheckDMARCRecord(dmarcRecords)
 
 	// Fetch DKIM Records
 	dkimRecords, err := dns_resolver.LookupDKIMRecords(*domain)
@@ -88,18 +87,65 @@ func main() {
 
 	// Display Results
 
-	// If summary flag is present, display the summary table
+	DisplaySummary(*domain, spfTestResults.AllTestsPassed, dmarcTestResults.AllTestsPassed, dkimPresent)
+	// If summary flag is present, don't show the remaining details
 	if *showSummary {
-		DisplaySummary(*domain, spfTestResults.AllTestsPassed, dmarcTestResults.AllTestsPassed, dkimPresent)
+
 		return
 	}
 
-	fmt.Printf("MX records: %v\n", mxRecords)
+	// MX Records
+	fmt.Printf("Domain: %s\n", *domain)
+	fmt.Printf("MX Records: \n")
+	if len(mxRecords) == 0 {
+		fmt.Println("- No MX records found")
+	} else {
+		for _, mxRecord := range mxRecords {
+			fmt.Printf("- %s\n", mxRecord)
+		}
+	}
 
-	fmt.Printf("SPF records: %v\n", spfRecords)
-	fmt.Println(spfSummary)
+	// SPF Records
+	fmt.Printf("SPF Records: \n")
+	if len(spfRecords) == 0 {
+		fmt.Println("- No SPF records found")
+	} else {
+		for _, spfRecord := range spfRecords {
+			fmt.Printf("- %s\n", spfRecord)
+		}
+	}
+	// SPF Analysis
+	fmt.Printf("SPF Analysis: \n")
+	fmt.Printf("- SPF Record is present: %v\n", spfTestResults.RecordFound)
+	fmt.Printf("- SPF Record is a single record: %v\n", spfTestResults.SingleRecord)
+	fmt.Printf("- SPF Record Qualifier is Hard Fail: %v\n", spfTestResults.QualifierHardFail)
 
-	fmt.Println(dmarcSummary)
+	// DMARC Records
+	fmt.Printf("DMARC Records: \n")
+	if len(dmarcRecords) == 0 {
+		fmt.Println("- No DMARC records found")
+	} else {
+		for _, dmarcRecord := range dmarcRecords {
+			fmt.Printf("- %s\n", dmarcRecord)
+		}
+	}
+
+	// DMARC Analysis
+	fmt.Printf("DMARC Analysis: \n")
+	fmt.Printf("- DMARC Record is present: %v\n", dmarcTestResults.RecordFound)
+	fmt.Printf("- DMARC Record is a single record: %v\n", dmarcTestResults.SingleRecord)
+	fmt.Printf("- DMARC Record Policy is Reject: %v\n", dmarcTestResults.PolicyReject)
+	fmt.Printf("- DMARC Record Subdomain Policy is Reject: %v\n", dmarcTestResults.SubdomainPolicyReject)
+	fmt.Printf("- DMARC Record Applies to 100%% of Mail: %v\n", dmarcTestResults.PctValid)
+
+	fmt.Printf("DKIM Records: \n")
+	if len(dkimRecords) == 0 {
+		fmt.Println("- No DKIM records found")
+	} else {
+		for _, dkimRecord := range dkimRecords {
+			fmt.Printf("- %s\n", dkimRecord)
+		}
+	}
 
 	// Output DKIM records with selectors
 	if len(dkimRecords) > 0 {
@@ -110,17 +156,4 @@ func main() {
 	} else {
 		fmt.Println("No DKIM records found.")
 	}
-
-	// Summarize results
-	fmt.Println("\nTest Results:")
-	fmt.Printf("1. SPF Record is present: %v\n", spfTestResults.RecordFound)
-	fmt.Printf("2. SPF Record is a single record: %v\n", len(spfRecords) == 1)
-	fmt.Printf("3. SPF Record has a Hard Fail Qualifier: %v\n", strings.Contains(spfSummary, "Hard fail qualifier: true"))
-	fmt.Printf("4. DMARC Record is present: %v\n", dmarcTestResults.RecordFound)
-	fmt.Printf("5. DMARC Record is a single record: %v\n", dmarcTestResults.SingleRecord)
-	fmt.Printf("6. DMARC Policy and Subdomain Policy is reject: %v\n", dmarcTestResults.PolicyReject && dmarcTestResults.SubdomainPolicyReject)
-	fmt.Printf("7. DKIM Record is present: %v\n", dkimPresent)
 }
-
-//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
-// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
